3ac0efca8dfa693ad1995a34bfb6768c058b6a48
Michael Bachmann <mbachmann@bbmsoft.net>
2021-09-30 13:04:35 +0000

    using centralized workspace configs

--- .gitpod/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
-curl https://gist.githubusercontent.com/babymotte/08d7f17a1aa70e372d745352cd3aabff/raw/2bc2e486decf4703647db88c2b3f003333ce33a3/01-git-prompt -o ~/.bashrc.d/01-git-prompt
-cargo install cargo-make &&
-    cargo make build-dev
